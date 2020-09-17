81ba280a03609ea16aa0c2b82badb66fe131ef5d
codingriver <wraithcorps@gmail.com>
2020-09-17 21:09:54 +0800

    URLHelper escape , EncodeURI ,  EncodeURIComponent

--- UnityTool/Assets/Test/URLHelperTest.cs
+++ UnityTool/Assets/Test/URLHelperTest.cs
@@ -12,14 +12,15 @@ public class URLHelperTest : MonoBehaviour
 
 
         Debug.Log(url);
-        Debug.Log(URLHelper.UrlEncode(url, encoding: Encoding.UTF8));
-        Debug.Log(URLHelper.EncodeURI(url));
-
-        Debug.Log(URLHelper.UrlDecode(URLHelper.EncodeURI(url), Encoding.UTF8));
-        Debug.Log(URLHelper.DecodeURI("h://+w.b+b%20d.%20m:9%200%200/o/t/i.a?%20key=%E4%BD%A0%E5%A5%BD%E5%90%97%EF%BC%9F#&%E5%A5%BD%E9%B8%AD%E5%A5%BD%E9%B8%AD%20%EF%BC%81!%20%EF%BC%8C#%EF%BC%8C$%EF%BC%8C&%EF%BC%8C'%EF%BC%8C(%EF%BC%8C)%EF%BC%8C*%EF%BC%8C+%EF%BC%8C,%EF%BC%8C-%EF%BC%8C.%EF%BC%8C/%EF%BC%8C:%EF%BC%8C;%EF%BC%8C=%EF%BC%8C?%EF%BC%8C@%EF%BC%8C_%EF%BC%8C~%EF%BC%8C#"));
-        Debug.Log(URLHelper.Escape(url));
-        Debug.Log(URLHelper.Unescape("h%3A//+w.b+b%20d.%20m%3A9%200%200/o/t/i.a%3F%20key%3D%u4F60%u597D%u5417%uFF1F%23%26%u597D%u9E2D%u597D%u9E2D%20%uFF01%21%20%uFF0C%23%uFF0C%24%uFF0C%26%uFF0C%27%uFF0C%28%uFF0C%29%uFF0C*%uFF0C+%uFF0C%2C%uFF0C-%uFF0C.%uFF0C/%uFF0C%3A%uFF0C%3B%uFF0C%3D%uFF0C%3F%uFF0C@%uFF0C_%uFF0C%7E%uFF0C%23"));
-        Debug.Log(URLHelper.Unescape(URLHelper.Escape(url)));
+        Debug.Log("UrlEncode:" + URLHelper.UrlEncode(url, encoding: Encoding.UTF8));
+        Debug.Log("UrlDecode:"+URLHelper.UrlDecode(URLHelper.EncodeURI(url), Encoding.UTF8));
+        Debug.Log("EncodeURI:"+URLHelper.EncodeURI(url));
+        Debug.Log("DecodeURI:"+URLHelper.DecodeURI("h://+w.b+b%20d.%20m:9%200%200/o/t/i.a?%20key=%E4%BD%A0%E5%A5%BD%E5%90%97%EF%BC%9F#&%E5%A5%BD%E9%B8%AD%E5%A5%BD%E9%B8%AD%20%EF%BC%81!%20%EF%BC%8C#%EF%BC%8C$%EF%BC%8C&%EF%BC%8C'%EF%BC%8C(%EF%BC%8C)%EF%BC%8C*%EF%BC%8C+%EF%BC%8C,%EF%BC%8C-%EF%BC%8C.%EF%BC%8C/%EF%BC%8C:%EF%BC%8C;%EF%BC%8C=%EF%BC%8C?%EF%BC%8C@%EF%BC%8C_%EF%BC%8C~%EF%BC%8C#"));
+        Debug.Log("EncodeURIComponent:" + URLHelper.EncodeURIComponent(url));
+        Debug.Log("DecodeURIComponent:" + URLHelper.DecodeURIComponent(URLHelper.EncodeURIComponent(url)));
+        Debug.Log("Escape:"+URLHelper.Escape(url));
+        Debug.Log("Unescape:" + URLHelper.Unescape("h%3A//+w.b+b%20d.%20m%3A9%200%200/o/t/i.a%3F%20key%3D%u4F60%u597D%u5417%uFF1F%23%26%u597D%u9E2D%u597D%u9E2D%20%uFF01%21%20%uFF0C%23%uFF0C%24%uFF0C%26%uFF0C%27%uFF0C%28%uFF0C%29%uFF0C*%uFF0C+%uFF0C%2C%uFF0C-%uFF0C.%uFF0C/%uFF0C%3A%uFF0C%3B%uFF0C%3D%uFF0C%3F%uFF0C@%uFF0C_%uFF0C%7E%uFF0C%23"));
+        Debug.Log("Unescape:"+URLHelper.Unescape(URLHelper.Escape(url)));
 
 
     }
