b129411e46b7dc94e0bdae3eff3cd986d093454f
codingriver <wraithcorps@gmail.com>
2020-09-18 18:42:55 +0800

    IdGenerater,生成分布式唯一id，不重复的

--- UnityTool/Assets/Scripts/Helper/TimeHelper.cs
+++ UnityTool/Assets/Scripts/Helper/TimeHelper.cs
@@ -4,44 +4,48 @@ namespace Codingriver
 {
 	public static class TimeHelper
     {
-        /// DateTime.Ticks:表示自 0001 年 1 月 1 日午夜 12:00:00 以来已经过的时间的以 100 毫微秒为间隔的间隔数,是一个很大的长整数，单位是 100 毫微秒。
-        /// 1 毫秒 = 10^-3 秒，
-        /// 1 微秒 = 10^-6 秒，
-        /// 1 毫微秒 = 10^-9 秒，
-        /// 100 毫微秒 = 10^-7 秒。
-
+        /*
+         DateTime.Ticks:表示自 0001 年 1 月 1 日午夜 12:00:00 以来已经过的时间的以 100 毫微秒为间隔的间隔数,是一个很大的长整数，单位是 100 毫微秒。
+         1 毫秒 = 10^-3 秒，
+         1 微秒 = 10^-6 秒，
+         1 毫微秒 = 10^-9 秒，
+         100 毫微秒 = 10^-7 秒。
+         */
 
         /// <summary>
         /// 自 0001 年 1 月 1 日午夜 12:00:00到现在经过多少个100微秒数，
         /// </summary>
         private static readonly long epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).Ticks;
-		/// <summary>
-		/// 客户端时间,毫秒
-        /// 表示自1970年1月1日0时0分0秒到现在的毫秒数
-		/// </summary>
-		/// <returns></returns>
-		public static long ClientNow()
-		{
-			return (DateTime.UtcNow.Ticks - epoch) / 10000;
-		}
+
+        /// <summary>
+        /// 客户端时间，单位 100 毫微秒
+        /// 自 1970 年 1 月 1 日午夜 12:00:00到现在经过多少个100微秒数，
+        /// </summary>
+        /// <returns></returns>
+        public static long Ticks()
+        {
+            return (DateTime.UtcNow.Ticks - epoch);
+        }
+
 
         /// <summary>
         /// 客户端时间，秒
         /// 表示自1970年1月1日0时0分0秒到现在的秒数
         /// </summary>
         /// <returns></returns>
-		public static long ClientNowSeconds()
+		public static long NowSeconds()
 		{
 			return (DateTime.UtcNow.Ticks - epoch) / 10000000;
 		}
 
         /// <summary>
         /// 客户端时间,毫秒
+        /// 表示自1970年1月1日0时0分0秒到现在的毫秒数
         /// </summary>
         /// <returns></returns>
         public static long Now()
 		{
-            return ClientNow();
-		}
+            return (DateTime.UtcNow.Ticks - epoch) / 10000;
+        }
     }
 }
---
+++ UnityTool/Assets/Test/IDGeneratorTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnityEngine;
+using Codingriver;
+using System.Text;
+using System.IO;
+public class IDGeneratorTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        long time = 0;
+        time = (long)((DateTime.UtcNow - new DateTime()).TotalMilliseconds);
+        Debug.Log($"从0001年开始的时间戳：{time}----{time.ToString("X2")}");
+        time = TimeHelper.Now();
+        Debug.Log($"从1970年开始的时间戳：{time}----{time.ToString("X2")}");
+        time = (long)((DateTime.UtcNow - new DateTime(2020, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds);
+        Debug.Log($"从2020年开始的时间戳：{time}----{time.ToString("X2")}");
+        Debug.Log("===================");
+
+        //yield return new WaitForSeconds(2);
+        //yield return new WaitForSeconds(2);
+        
+        StringBuilder b = new StringBuilder();
+        Debug.Log(DateTime.Now.ToLocalTime().ToString("yyyy-MM-dd HH:mm:ss:fff"));
+        for (int i = 0; i < 1000000; i++)
+        {
+            long id = TimeHelper.Now();
+
+            //DateTime dt= Jan1st1970.AddMilliseconds(id);
+            //b.Append($"{id}---{dt.ToLocalTime().ToString("yyyy-MM-dd HH:mm:ss:fff")}\n");
+            //b.Append($"{id.ToString("X2")}\n");
+
+        }
+        Debug.Log(DateTime.Now.ToLocalTime().ToString("yyyy-MM-dd HH:mm:ss:fff"));
+        //yield return new WaitForSeconds(1);
+        File.WriteAllText(".time1.log", b.ToString());
+        b.Clear();
+        
+        //yield return new WaitForSeconds(2);
+        Debug.Log(DateTime.Now.ToLocalTime().ToString("yyyy-MM-dd HH:mm:ss:fff"));
+        for (int i = 0; i < 1000000; i++)
+        {
+            long id = IdGenerater.GenerateId();
+            //b.Append($"{id.ToString("X2")} \n");
+            //b.Append($"{id}, {IdGenerater.SnowflakeId.AnalyzeId(id)} \n");
+        }
+        Debug.Log(DateTime.Now.ToLocalTime().ToString("yyyy-MM-dd HH:mm:ss:fff"));
+        //yield return new WaitForSeconds(1);
+        Debug.Log(b.ToString());
+        File.WriteAllText(".time2.log", b.ToString());
+        //yield break;
+    }
+
+}
