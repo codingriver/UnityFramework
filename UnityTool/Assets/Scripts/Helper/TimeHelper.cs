19982735af33bdf85052dcfc379813c92da73d72
codingriver <wraithcorps@gmail.com>
2020-09-18 18:50:19 +0800

    TimeHelper

--- UnityTool/Assets/Scripts/Helper/TimeHelper.cs
+++ UnityTool/Assets/Scripts/Helper/TimeHelper.cs
@@ -12,10 +12,11 @@ namespace Codingriver
          100 毫微秒 = 10^-7 秒。
          */
 
+        private static readonly DateTime epoch1970 = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
         /// <summary>
         /// 自 0001 年 1 月 1 日午夜 12:00:00到现在经过多少个100微秒数，
         /// </summary>
-        private static readonly long epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).Ticks;
+        private static readonly long epochTicks = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).Ticks;
 
         /// <summary>
         /// 客户端时间，单位 100 毫微秒
@@ -24,7 +25,7 @@ namespace Codingriver
         /// <returns></returns>
         public static long Ticks()
         {
-            return (DateTime.UtcNow.Ticks - epoch);
+            return (DateTime.UtcNow.Ticks - epochTicks);
         }
 
 
@@ -35,7 +36,7 @@ namespace Codingriver
         /// <returns></returns>
 		public static long NowSeconds()
 		{
-			return (DateTime.UtcNow.Ticks - epoch) / 10000000;
+			return (DateTime.UtcNow.Ticks - epochTicks) / 10000000;
 		}
 
         /// <summary>
@@ -45,7 +46,26 @@ namespace Codingriver
         /// <returns></returns>
         public static long Now()
 		{
-            return (DateTime.UtcNow.Ticks - epoch) / 10000;
+            return (DateTime.UtcNow.Ticks - epochTicks) / 10000;
+        }
+
+        /// <summary>
+        /// 时间戳转时间显示
+        /// </summary>
+        /// <param name="milliseconds">时间戳 毫秒</param>
+        /// <returns></returns>
+        public static string StampToDate(long milliseconds)
+        {
+            return epoch1970.AddMilliseconds(milliseconds).ToLocalTime().ToString("yyyy-MM-dd HH:mm:ss:fff");
+        }
+        /// <summary>
+        /// 时间戳转时间显示
+        /// </summary>
+        /// <param name="seconds">时间戳 秒</param>
+        /// <returns></returns>
+        public static string StampToDateBySeconds(long seconds)
+        {
+            return epoch1970.AddMilliseconds(seconds).ToLocalTime().ToString("yyyy-MM-dd HH:mm:ss:fff");
         }
     }
 }
