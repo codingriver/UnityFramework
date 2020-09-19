ad9a2de3b0485a1487a0372f6e7d102dbfafc74c
codingriver <wraithcorps@gmail.com>
2020-09-19 19:22:00 +0800

    monitor

---
+++ UnityTool/Assets/Scripts/Helper/AutoMonitor.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Threading;
+
+namespace Codingriver
+{
+    class AutoMonitor
+    {
+
+        private object locker = new object();
+        /// <summary>
+        /// true:WaitOne不阻塞，只一次
+        /// false:WaitOne阻塞
+        /// </summary>
+        private bool unblock;
+        private short blockCount = 0;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="initialState"></param>
+        public AutoMonitor(bool initialState)
+        {
+            this.unblock = initialState;
+
+        }
+
+        public void Set()
+        {
+            lock (locker)
+            {
+                if (blockCount > 0)
+                {
+                    Monitor.Pulse(locker);
+                }
+                else
+                {
+                    unblock = true;
+                }
+            }
+
+
+        }
+
+        public void WaitOne()
+        {
+            if (unblock)
+            {
+                unblock = false;
+                return;
+            }
+
+            lock (locker)
+            {
+                blockCount++;
+                Monitor.Wait(locker);
+                blockCount--;
+            }
+
+            /*
+            bool lockState = false;
+            try
+            {
+                Monitor.Enter(obj, ref lockState);
+
+                Monitor.Wait(obj);
+            }
+            finally
+            {
+                if(lockState)
+                {
+                    Monitor.Exit(obj);
+                }
+            }
+             */
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="millisecondsTimeout">线程进入就绪队列之前等待的毫秒数</param>
+        /// <returns></returns>
+        public bool WaitOne(int millisecondsTimeout)
+        {
+            bool isNotTimeOut = true;
+            if (unblock)
+            {
+                unblock = false;
+                return isNotTimeOut;
+            }
+
+            lock (locker)
+            {
+                blockCount++;
+                isNotTimeOut = Monitor.Wait(locker, millisecondsTimeout);
+                blockCount--;
+            }
+            return isNotTimeOut;
+        }
+
+        public void Reset()
+        {
+            unblock = false;
+        }
+
+    }
+}
---
+++ UnityTool/Assets/Scripts/Helper/ManualMonitor.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Threading;
+
+namespace Codingriver
+{
+    class ManualMonitor
+    {
+
+        private object locker = new object();
+
+        private bool unblock;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="initialState"></param>
+        public ManualMonitor(bool initialState)
+        {
+            unblock = initialState;
+
+        }
+
+        public void Set()
+        {
+            lock (locker)
+            {
+                Monitor.PulseAll(locker);
+                unblock = true;
+
+            }
+
+        }
+
+        public void WaitOne()
+        {
+            if (unblock)
+            {
+                return;
+            }
+
+            lock (locker)
+            {
+                Monitor.Wait(locker);
+
+            }
+            /*
+                        bool lockState = false;
+                        try
+                        {
+                            Monitor.Enter(obj, ref lockState);
+
+                            Monitor.Wait(obj);
+                        }
+                        finally
+                        {
+                            if (lockState)
+                            {
+                                Monitor.Exit(obj);
+                            }
+                        }
+             */
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="millisecondsTimeout">线程进入就绪队列之前等待的毫秒数</param>
+        /// <returns></returns>
+        public bool WaitOne(int millisecondsTimeout)
+        {
+            bool isNotTimeOut = true;
+
+            if (!unblock)
+            {
+                return isNotTimeOut;
+            }
+            lock (locker)
+            {
+                isNotTimeOut = Monitor.Wait(locker, millisecondsTimeout);
+
+            }
+            return isNotTimeOut;
+        }
+
+        public void Reset()
+        {
+            unblock = false;
+        }
+    }
+}
